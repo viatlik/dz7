c3e488c810c6fa6076c66fe863f0b13c00591366
vitalik <vitalik-s-hedgehog@yandex.ru>
2022-12-29 20:47:59 +0300

    третье задание

---
+++ dz 3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintArray (int[, ]matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[, ]matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+int [, ] matrix = new int [3, 3];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    int average = 0;
+
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        average = average + matrix [j,i];
+    }
+    Console.WriteLine($" Среднее арифметическое элементов {i+1} = {((float)average / matrix.GetLength(0))}");
+}
