83e80cfdd243e00291ce9c4ce90a6548fb2ed261
vitalik <vitalik-s-hedgehog@yandex.ru>
2022-12-29 20:36:04 +0300

    первое  задание

---
+++ dz 1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами от -10,0 до 10,0.
+
+// m = 3, n = 4.
+
+// 0,5 7,0 -2,0 -0,2
+// 1,0 -3,3 8,0 -9,9
+// 8,0 7,8 -7,1 9,0
+
+
+double[,] arr = new double[3, 4];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(-10, 10) + new Random().NextDouble();
+        arr[i,j] = Math.Round(arr[i,j], 1);
+    }
+}
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+     for (int j = 0; j < arr.GetLength(1); j++)
+     {
+         Console.Write(arr[i, j] + " ");
+     }
+     Console.WriteLine();
+ }
