57678e2f3d3a2f0cbeb753e215e57268ab5f96a6
vitalik <vitalik-s-hedgehog@yandex.ru>
2022-12-29 20:46:03 +0300

    второе  задание

---
+++ dz 2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число, и возвращает индексы числа в двумерном массиве или же указание, что такого элемента нет.
+
+void PrintArray (int[, ]matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[, ]matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+int [, ] matrix = new int [5, 5];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+Console.WriteLine("Введите число: ");
+int findNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i,j] == findNumber)
+        {
+            Console.WriteLine("число в ячейке: " + i + "," + j);
+        }
+        else
+        {
+            Console.WriteLine("Ваше число отсутствует в массиве");
+        }
+    }
+}
